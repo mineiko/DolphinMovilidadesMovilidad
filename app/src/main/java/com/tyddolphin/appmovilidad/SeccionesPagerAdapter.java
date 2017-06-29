34de4c190b8da9a68bae83bd8f5750226d4de76f
alxs1596 <alxs1596@hotmail.com>
2017-06-29 10:52:31 -0500

    Pager Adapter mantener fragments activos

--- app/src/main/java/com/tyddolphin/appmovilidad/SeccionesPagerAdapter.java
+++ app/src/main/java/com/tyddolphin/appmovilidad/SeccionesPagerAdapter.java
@@ -19,41 +19,14 @@ class SeccionesPagerAdapter extends FragmentPagerAdapter {
             new FragmentConfiguracion(),
             new FragmentPreguntasFrecuentes()
     };
-    Fragment FMA;
-    Fragment FA;
-    Fragment FCM;
-    Fragment FC;
-    Fragment FPF;
+
 
     @Override
     public Fragment getItem(int position) {
-        switch (position){
-            case 0:
-                if(FMA == null)
-                    FMA = fragments[position];
-                return FMA;
-            case 1:
-                if(FA == null)
-                    FA = fragments[position];
-                return FA;
-            case 2:
-                if(FCM == null)
-                    FCM= fragments[position];
-                return FCM;
-            case 3:
-                if(FC== null)
-                    FC= fragments[position];
-                return FC;
-            case 4:
-                if(FPF == null)
-                    FPF= fragments[position];
-                return FPF;
-        }
-        return null;
-
+        return fragments[position];
     }
     public int getCount() {
-        return 5;
+        return fragments.length;
     }
 
 }
